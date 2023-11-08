b7b0a864947b79d8862eceac00c6ba88e4beef8f
Christian <c.espinoza.code@gmail.com>
2023-11-08 10:17:02 -0500

    Modal imported in root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+'use client';
+
 import { UserButton } from '@clerk/nextjs';
+import { Modal } from '@/components/ui/modal';
 
 function SetupPage() {
   return (
     <div className="p-4 bg-black">
-      <UserButton afterSignOutUrl='/' />
+      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
+        Children
+        {/* <UserButton afterSignOutUrl="/" /> */}
+      </Modal>
     </div>
   );
 }
