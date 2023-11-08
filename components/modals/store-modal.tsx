6f4d4c0a49f4131256ab83d6749d2b65407810d9
Christian <c.espinoza.code@gmail.com>
2023-11-08 15:22:44 -0500

    store modal now uses desctructure

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -4,14 +4,14 @@ import { useStoreModal } from '@/hooks/use-store-modal';
 import { Modal } from '@/components/ui/modal';
 
 export const StoreModal = () => {
-  const storeModal = useStoreModal();
+  const { isOpen, onClose } = useStoreModal();
 
   return (
     <Modal
       title="Create Store"
       description="Add a new store to manage products and categories"
-      isOpen={storeModal.isOpen}
-      onClose={storeModal.onClose}
+      isOpen={isOpen}
+      onClose={onClose}
     >
       Future Create Store From
     </Modal>
