5bde4b7217feaf372de939092834ea5d54fa07ab
Christian <c.espinoza.code@gmail.com>
2023-11-09 17:27:18 -0500

    route for api to route store POST method created

---
+++ app/api/stores/route.ts
@@ -0,0 +1,33 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+import prismadb from '@/lib/prismadb';
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const { name } = body;
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 });
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId,
+      }
+    })
+
+    return new NextResponse(JSON.stringify(store), { status: 200 });
+  } catch (error) {
+    console.log('[STORES_POST]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
