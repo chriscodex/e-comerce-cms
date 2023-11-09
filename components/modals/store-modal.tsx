94b8fda5bc589db7be9c43587f8609745ed9e84f
Christian <c.espinoza.code@gmail.com>
2023-11-09 16:25:44 -0500

    form & zod configured ofr store modal

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,30 @@
 'use client';
 
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import { useStoreModal } from '@/hooks/use-store-modal';
 import { Modal } from '@/components/ui/modal';
+import { Form } from '@/components/ui/form';
+
+const formSchema = z.object({
+  name: z.string().min(1, 'Required'),
+});
 
 export const StoreModal = () => {
   const { isOpen, onClose } = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
 
   return (
     <Modal
@@ -13,7 +33,13 @@ export const StoreModal = () => {
       isOpen={isOpen}
       onClose={onClose}
     >
-      Future Create Store From
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}></form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
