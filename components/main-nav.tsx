6b179745c476b2f2c5dbd4d23e8959caa4497ed2
Christian <c.espinoza.code@gmail.com>
2023-11-10 23:10:59 -0500

    Main nav component created & added to navbar

---
+++ components/main-nav.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  return <div>Main Navgation</div>;
+}
+
+export { MainNav };
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,19 @@
+import { UserButton } from '@clerk/nextjs';
+import { MainNav } from './main-nav';
+
 function Navbar() {
   return (
-    <div>
-      This is a component
+    <div className="border-b ">
+      <div className="flex h-16 items-center px-4">
+        <div>This will be a store switcher</div>
+        <MainNav />
+        <div className="ml-auto flex items-center scale-x-4">
+          <UserButton afterSignOutUrl="/" />
+          MainNav
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
