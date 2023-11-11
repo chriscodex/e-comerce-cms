5f18042e7dd809893ef6d5ea03f8488d3a8dea7a
Christian <c.espinoza.code@gmail.com>
2023-11-10 23:21:12 -0500

    Setting button configured

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,13 @@
 import { UserButton } from '@clerk/nextjs';
-import { MainNav } from './main-nav';
+
+import { MainNav } from '@/components/main-nav';
 
 function Navbar() {
   return (
     <div className="border-b ">
       <div className="flex h-16 items-center px-4">
         <div>This will be a store switcher</div>
-        <MainNav />
+        <MainNav className='mx-6'/>
         <div className="ml-auto flex items-center scale-x-4">
           <UserButton afterSignOutUrl="/" />
           MainNav
