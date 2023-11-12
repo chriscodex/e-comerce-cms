23a70ac985afb0fef3577e1c9e11dd2703bf5f1f
Christian <c.espinoza.code@gmail.com>
2023-11-11 23:34:03 -0500

    popover trigger interface implemented in store switcher

---
+++ components/store-switcher.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Store } from '@prisma/client'
+
+import { PopoverTrigger } from '@/components/ui/popover'
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
+
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  items: Store[]
+}
+
+function StoreSwitcher({
+  className, items = []
+} : StoreSwitcherProps) {
+  return (
+    <div>
+      Store Switcher
+    </div>
+  )
+}
+
+export { StoreSwitcher }
