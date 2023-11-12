04bfd9ef7fa85ea142e39240830b18e99e6f0a69
Christian <c.espinoza.code@gmail.com>
2023-11-12 15:32:52 -0500

    rutes protected logic added to settings page

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,15 +1,33 @@
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
+
+import prismadb from '@/lib/prismadb';
+
 interface SettingsPageProps {
   params: {
-    storeId: string
+    storeId: string;
+  };
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+  const {userId} = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
   }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId
+    }
+  })
+
+  if (!store) {
+    redirect('/');
+  }
+
+  return <div className='flex-col'>Hello Settings</div>;
 }
 
-function SettingsPage() {
-  return (
-    <div>
-      Hello Settings
-    </div>
-  )
-}
-
-export default SettingsPage
+export default SettingsPage;
