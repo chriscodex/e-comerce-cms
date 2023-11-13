ecb19749b072b66322b5541f27090d7c974cb205
Christian <c.espinoza.code@gmail.com>
2023-11-12 19:25:00 -0500

    zod configuration added to settings form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,16 +1,31 @@
 'use client';
 
+import * as z from 'zod';
 import { Store } from '@prisma/client';
+import { Trash } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Heading } from '@/components/ui/heading';
 import { Button } from '@/components/ui/button';
-import { Trash } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -19,6 +34,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
           <Trash className="h-4 w-4" />
         </Button>
       </div>
+      <Separator />
     </>
   );
 };
