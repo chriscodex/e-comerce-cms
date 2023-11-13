321d51969320243ad33a7673ed883f83cc25bb8d
Christian <c.espinoza.code@gmail.com>
2023-11-12 19:15:53 -0500

    fix: Setting heading ahora está centrado

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -3,6 +3,8 @@
 import { Store } from '@prisma/client';
 
 import { Heading } from '@/components/ui/heading';
+import { Button } from '@/components/ui/button';
+import { Trash } from 'lucide-react';
 
 interface SettingsFormProps {
   initialData: Store;
@@ -12,6 +14,9 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   return (
     <div className="flex items-center justify-between">
       <Heading title="Settings" description="Manage store preferences" />
+      <Button variant="destructive" size="icon" onClick={() => {}}>
+        <Trash className="h-4 w-4" />
+      </Button>
     </div>
   );
 };
