1a101106bb22de2a136ae50a32aff949fb7d0276
Christian <c.espinoza.code@gmail.com>
2023-11-14 10:20:55 -0500

    new page billboard initialized

---
+++ app/(root)/(routes)/billboards/page.tsx
@@ -0,0 +1,10 @@
+const Billboards = () => {
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6"></div>
+      Billboards
+    </div>
+  );
+};
+
+export default Billboards;
