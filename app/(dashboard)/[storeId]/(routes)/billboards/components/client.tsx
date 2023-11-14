e02e2c427567470ae9d53eb589d9eab34e5829a0
Christian <c.espinoza.code@gmail.com>
2023-11-14 10:25:18 -0500

    billboard page moved into StoreId dinamic page

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const BillboardClient = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        BillboardClient
+      </div>
+    </>
+  );
+};
+
+export default BillboardClient;
