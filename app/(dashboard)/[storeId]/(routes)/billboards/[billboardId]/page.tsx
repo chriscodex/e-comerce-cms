97144ddb68c967a5e122b46e227f0057fd65defd
Christian <c.espinoza.code@gmail.com>
2023-11-14 11:57:41 -0500

    billboard form created

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,9 +1,23 @@
-const BillboardPage = () => {
+import prismadb from '@/lib/prismadb';
+
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  const billboard = await prismadb.billboard.findUnique({
+    where: {
+      id: params.billboardId,
+    },
+  });
+
   return (
-    <div>
-      BillboardPage
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        
+      </div>
     </div>
   );
-}
+};
 
-export default BillboardPage
+export default BillboardPage;
