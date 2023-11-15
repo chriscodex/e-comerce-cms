f7d40146044ac45fbc6f5c3d6f26542c9dc940ec
Christian <c.espinoza.code@gmail.com>
2023-11-14 23:58:47 -0500

    badges for apis added

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -1,5 +1,6 @@
 import { Alert, AlertTitle } from '@/components/ui/alert';
 import { Server } from 'lucide-react';
+import { Badge } from '@/components/ui/badge';
 
 interface ApiAlertProps {
   title: string;
@@ -25,7 +26,10 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
   return (
     <Alert>
       <Server className="h-4 w-4" />
-      <AlertTitle className="flex items-center gap-x-2">{title}</AlertTitle>
+      <AlertTitle className="flex items-center gap-x-2">
+        {title}
+        <Badge>{textMap[variant]}</Badge>
+      </AlertTitle>
     </Alert>
   );
 };
