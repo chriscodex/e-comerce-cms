25cdd264f37f1295dd346c123e3b110fb4aa4b55
Christian <c.espinoza.code@gmail.com>
2023-11-16 14:39:40 -0500

    onDelete method for billboard form implemented

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -86,12 +86,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/stores/${params.storeId}`);
+      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
       router.refresh();
       router.push('/');
-      toast.success('Store deleted successfully.');
+      toast.success('Billboard deleted successfully.');
     } catch (error) {
-      toast.error('Make sure you removed all products and categories first.');
+      toast.error('Make sure you removed all categories using this billboard first.');
     } finally {
       setLoading(false);
       setOpen(false);
