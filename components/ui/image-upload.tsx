27bf92ed689ce33eb62b9bfdfc3cda5670d7b862
Christian <c.espinoza.code@gmail.com>
2023-11-16 14:32:16 -0500

    ImageUpload component implemented on billboard form

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -43,26 +43,36 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
             className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
           >
             <div className="z-10 absolute top-2 ring-2">
-              <Button type="button" onClick={() => onRemove(url)} variant={'destructive'}>
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                variant={'destructive'}
+              >
                 <Trash className="w-4 h-4" />
               </Button>
             </div>
-            <Image fill className='object-cover' alt='Image' src={url}/>
+            <Image fill className="object-cover" alt="Image" src={url} />
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset='cs5potng'>
-          {({open}) => {
-            const onClick = () => {
-              open();
-            }
+      <CldUploadWidget onUpload={onUpload} uploadPreset="cs5potng">
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
 
-            return (
-              <Button type='button' disabled={disabled} variant={'secondary'} onClick={onClick}>
-                <ImagePlus className='h-4 w-4 mr-2'/>
-              </Button>
-            )
-          }}
+          return (
+            <Button
+              type="button"
+              disabled={disabled}
+              variant={'secondary'}
+              onClick={onClick}
+            >
+              <ImagePlus className="h-4 w-4 mr-2" />
+              Upload an Image
+            </Button>
+          );
+        }}
       </CldUploadWidget>
     </div>
   );
