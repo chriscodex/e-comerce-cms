8a3a7b2cb8f3895fdc43c578d490e7704502446b
Christian <c.espinoza.code@gmail.com>
2023-11-17 18:43:26 -0500

    dropdown menu created with shadcn

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,4 +1,14 @@
-export const CellAction = () => {
+'use client';
+
+import { BillboardColumn } from './columns';
+
+interface CellActionColumn {
+  data: BillboardColumn
+}
+
+export const CellAction: React.FC<CellActionColumn> = ({
+  data
+}) => {
   return (
     <div>Action</div>
   )
