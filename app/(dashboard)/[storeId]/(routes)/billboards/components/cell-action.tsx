494f5197ca10fe412e52232a40a2e0c779d2e109
Christian <c.espinoza.code@gmail.com>
2023-11-17 18:46:48 -0500

    dropdown menu icon inserted

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,15 +1,31 @@
 'use client';
 
+import {
+  DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+} from '@/components/ui/dropdown-menu';
+
 import { BillboardColumn } from './columns';
+import { Button } from '@/components/ui/button';
+import { MoreHorizontal } from 'lucide-react';
 
 interface CellActionColumn {
-  data: BillboardColumn
+  data: BillboardColumn;
 }
 
-export const CellAction: React.FC<CellActionColumn> = ({
-  data
-}) => {
+export const CellAction: React.FC<CellActionColumn> = ({ data }) => {
   return (
-    <div>Action</div>
-  )
-}
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={'ghost'} className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
