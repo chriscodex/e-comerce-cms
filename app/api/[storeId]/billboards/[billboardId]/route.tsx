a26aa068ad19c07180a83dd10a2ef1d528a9a831
Christian <c.espinoza.code@gmail.com>
2023-11-17 21:13:19 -0500

    fix: delete endpoint now delete elements

--- app/api/[storeId]/billboards/[billboardId]/route.tsx
+++ app/api/[storeId]/billboards/[billboardId]/route.tsx
@@ -80,39 +80,39 @@ export async function PATCH(
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { storeId: string, billboardId: string } }
+  { params }: { params: { billboardId: string, storeId: string } }
 ) {
   try {
     const { userId } = auth();
 
     if (!userId) {
-      return new NextResponse('Unauthenticated', { status: 401 });
+      return new NextResponse("Unauthenticated", { status: 403 });
     }
 
     if (!params.billboardId) {
-      return new NextResponse('Billboard ID is required', { status: 400 });
+      return new NextResponse("Billboard id is required", { status: 400 });
     }
 
     const storeByUserId = await prismadb.store.findFirst({
       where: {
         id: params.storeId,
         userId,
-      },
-    })
+      }
+    });
 
     if (!storeByUserId) {
-      return new NextResponse('Unauthorized', { status: 403 });
+      return new NextResponse("Unauthorized", { status: 405 });
     }
 
-    const billboard = await prismadb.store.deleteMany({
+    const billboard = await prismadb.billboard.delete({
       where: {
         id: params.billboardId,
-      },
-    })
-
+      }
+    });
+  
     return NextResponse.json(billboard);
   } catch (error) {
     console.log('[BILLBOARD_DELETE]', error);
-    return new NextResponse('Internal Error', { status: 500 });
+    return new NextResponse("Internal error", { status: 500 });
   }
-}
+};
