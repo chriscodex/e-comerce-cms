6654c357902033e4f1bf068501d7022e149a3277
Christian <c.espinoza.code@gmail.com>
2023-11-18 12:46:06 -0500

    Post endpoint for categories added

---
+++ app/api/[storeId]/categories/route.ts
@@ -0,0 +1,79 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+import prismadb from '@/lib/prismadb';
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const { name, billboardId } = body;
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 });
+    }
+
+    if (!billboardId) {
+      return new NextResponse('BillboardId is required', { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse('Store ID is required', { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    })
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const category = await prismadb.category.create({
+      data: {
+        name,
+        billboardId,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log('[CATEGORIES_POST]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse('Store ID is required', { status: 400 });
+    }
+
+    const billboards = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId,
+      }
+    });
+
+    return NextResponse.json(billboards);
+  } catch (error) {
+    console.log('[BILLBOARDS_GET]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
