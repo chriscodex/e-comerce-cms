0166784d3c49c2b3124386edfe1ed58d48d9bbfa
Christian <c.espinoza.code@gmail.com>
2023-11-19 21:13:22 -0500

    products page created

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -1,32 +1,49 @@
-import {format} from 'date-fns'
+import { format } from "date-fns";
 
-import prismadb from '@/lib/prismadb';
-import BillboardClient from './components/client';
-import { BillboardColumn } from './components/columns';
+import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
 
-const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
-  const billboards = await prismadb.billboard.findMany({
+import { ProductsClient } from "./components/client";
+import { ProductColumn } from "./components/columns";
+
+const ProductsPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  const products = await prismadb.product.findMany({
     where: {
-      storeId: params.storeId,
+      storeId: params.storeId
+    },
+    include: {
+      category: true,
+      size: true,
+      color: true,
     },
     orderBy: {
-      createdAt: 'desc',
+      createdAt: 'desc'
     }
   });
 
-  const formatedBillboards: BillboardColumn[] = billboards.map((item) => ({
+  const formattedProducts: ProductColumn[] = products.map((item) => ({
     id: item.id,
-    label: item.label,
-    createdAt: format(item.createdAt, 'MMMM do, yyyy')
-  }))
+    name: item.name,
+    isFeatured: item.isFeatured,
+    isArchived: item.isArchived,
+    price: formatter.format(item.price.toNumber()),
+    category: item.category.name,
+    size: item.size.name,
+    color: item.color.value,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formatedBillboards}/>
+        <ProductsClient data={formattedProducts} />
       </div>
     </div>
   );
 };
 
-export default BillboardsPage;
+export default ProductsPage;
