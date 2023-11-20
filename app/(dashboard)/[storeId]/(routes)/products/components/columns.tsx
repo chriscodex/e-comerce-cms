2206b1123f9189f1c7660c8709d872c0e7ab1377
Christian <c.espinoza.code@gmail.com>
2023-11-19 21:14:19 -0500

    columns configured for products page

--- app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -1,27 +1,62 @@
-'use client';
+"use client"
 
-import { ColumnDef } from '@tanstack/react-table';
-import { CellAction } from './cell-action';
+import { ColumnDef } from "@tanstack/react-table"
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-export type BillboardColumn = {
-  id: string;
-  label: string;
+import { CellAction } from "./cell-action"
+
+export type ProductColumn = {
+  id: string
+  name: string;
+  price: string;
+  category: string;
+  size: string;
+  color: string;
   createdAt: string;
-};
+  isFeatured: boolean;
+  isArchived: boolean;
+}
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<ProductColumn>[] = [
   {
-    accessorKey: 'label',
-    header: 'Label',
+    accessorKey: "name",
+    header: "Name",
   },
   {
-    accessorKey: 'createdAt',
-    header: 'Date',
+    accessorKey: "isArchived",
+    header: "Archived",
   },
   {
-    id: 'actions',
-    cell: ({ row }) => <CellAction data={row.original} />,
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "color",
+    header: "Color",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.color}
+        <div className="h-6 w-6 rounded-full border" style={{ backgroundColor: row.original.color }} />
+      </div>
+    )
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
   },
 ];
